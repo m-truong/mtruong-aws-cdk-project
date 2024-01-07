3d1160edfb3a0e019b619a3e6b30d5d80160b903
Michael Truong <59580072+m-truong@users.noreply.github.com>
2024-01-07 16:09:49 -0500

    Add cdk stack and prettierrc

--- lib/mtruong-aws-cdk-project-stack.ts
+++ lib/mtruong-aws-cdk-project-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
 export class MtruongAwsCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,9 @@ export class MtruongAwsCdkProjectStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MtruongAwsCdkProjectQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+    });
   }
 }
